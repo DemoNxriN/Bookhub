1c285d41cdcfab74d654c3b1e15e7c51023ea6c2
DemoNxriN <100371984+DemoNxriN@users.noreply.github.com>
2024-02-06 03:33:00 +0100

    Update

--- php/login.php
+++ php/login.php
@@ -1,32 +1,11 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Iniciar Sesión</title>
-    <link rel="icon" type="image/png" sizes="64x64" href="/img/favicon.ico">
-    <link rel="stylesheet" href="./css/style.css">
-</head>
-<body>
-    <h1>Iniciar Sesión</h1>
-
-    <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Aquí iría tu lógica de manejo de formularios
-        // Puedes acceder a los datos del formulario con $_POST
-        // Por ejemplo, $_POST["username"], $_POST["password"], etc.
-        // Realizar validaciones y procesos de inicio de sesión aquí
-    }
-    ?>
-
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        <!-- Aquí coloca los campos del formulario (nombre de usuario, contraseña, etc.) -->
-        <label for="username">Nombre de usuario:</label>
-        <input type="text" id="username" name="username" required>
-
-        <label for="password">Contraseña:</label>
-        <input type="password" id="password" name="password" required>
-
-        <button type="submit">Iniciar Sesión</button>
-    </form>
-</body>
-</html>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    
+    // Impresion de datos
+    echo "Inicio de sesión exitoso:<br>";
+    echo "Usuario: $username<br>";
+    echo "Contraseña: $password<br>";
+}
+?>
--- php/register.php
+++ php/register.php
@@ -1,29 +1,11 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Registro</title>
-    <link rel="icon" type="image/png" sizes="64x64" href="/img/favicon.ico">
-    <link rel="stylesheet" href="./css/style.css">
-</head>
-<body>
-    <h1>Registro</h1>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
-    <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        
-    }
-    ?>
-
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        <!-- Aquí coloca los campos del formulario (nombre de usuario, contraseña, etc.) -->
-        <label for="username">Nombre de usuario:</label>
-        <input type="text" id="username" name="username" required>
-
-        <label for="password">Contraseña:</label>
-        <input type="password" id="password" name="password" required>
-
-        <button type="submit">Registrarse</button>
-    </form>
-</body>
-</html>
+    // Impresion de datos
+    echo "Usuario registrado:<br>";
+    echo "Usuario: $username<br>";
+    echo "Contraseña: $password<br>";
+}
+?>
