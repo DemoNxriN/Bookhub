0f289d5dd2afc5f1930e9074e3f8cd62081ed5fd
DemoNxriN <100371984+DemoNxriN@users.noreply.github.com>
2024-02-16 03:22:39 +0100

    Update

--- php/comments.php
+++ php/comments.php
@@ -1,5 +1,5 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["comment"]) && isset($_GET["book"])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["comment"])) {
     // Obtener y limpiar el comentario del formulario
     $commentText = trim($_POST["comment"]);
     $commentText = htmlspecialchars($commentText);
@@ -22,34 +22,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["comment"]) && isset($_
         }
 
         // Preparar la consulta SQL de manera segura para insertar el comentario
-        $stmt = $conn->prepare("INSERT INTO comentarios (texto, libro_id) VALUES (?, ?)");
-        $stmt->bind_param("si", $commentText, $bookParam);
+        $stmt = $conn->prepare("INSERT INTO comentarios (texto) VALUES (?)");
+        $stmt->bind_param("s", $commentText);
         $stmt->execute();
-
-        // Después de insertar el comentario, recuperar la información del comentario recién insertado
-        $insertedCommentId = $conn->insert_id;
-        $sql = "SELECT * FROM `comentarios` WHERE `id` = $insertedCommentId";
-        $result = $conn->query($sql);
-
-        // Verificar si hay resultados
-        if ($result->num_rows > 0) {
-            // Obtener la información del comentario
-            $commentInfo = $result->fetch_assoc();
-
-            // Devolver la información del comentario en formato JSON
-            echo json_encode($commentInfo);
-        } else {
-            // Si no hay comentarios, imprimir un mensaje de error
-            echo json_encode(array('error' => 'No se pudo recuperar el comentario recién insertado.'));
-        }
 
         // Cerrar la conexión a la base de datos
         $stmt->close();
         $conn->close();
 
         // Redirigir al archivo HTML después de insertar el comentario
-        // header("Location: book$bookParam.html");
-        // exit();  // No es necesario redirigir aquí ya que estamos manejando la respuesta mediante AJAX
+        header("Location: book$bookParam.html");
+        exit();
     } else {
         echo "Por favor, introduce un comentario válido.";
     }
