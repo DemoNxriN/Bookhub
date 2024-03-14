02ddfa3b278fceff6ac9b652c9e9536990e3eb49
NO0ZA <biel.baroc@educem.net>
2024-03-14 19:19:45 +0100

    update sinopsis 2

--- php/book2.php
+++ php/book2.php
@@ -26,7 +26,9 @@
                 <img class="card-img" src="../img/alicia a traves del espejo.jpg" alt="Descripción de la imagen">
                 <div class="card-text">
                     <h2>Alicia a traves del espejo</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet autem, sunt praesentium deserunt deleniti consectetur, nostrum magnam officiis eos soluta, inventore consequuntur necessitatibus? Commodi iste eius eveniet nulla itaque sunt..</p>
+                    <p>Cuando Alicia (Mia Wasikowska) despierta en el País de las Maravillas, la joven protagonista deberá viajar a través de este mundo mágico e intentar cambiar el pasado con la ayuda de un enigmático artilugio conocido como la cronosfera, todo con el fin de rescatar a su amigo el Sombrerero Loco (Johnny Depp). Solo así podrá detener al malvado Tiempo (Sacha Baron Cohen) antes de que se cumpla el plazo marcado por el reloj y el País de las Maravillas se convierta en un mundo estéril y sin vida. Y, por si fuera poco, también estará en su mano frenar los malvados planes de la Reina Roja (Helena Bonham Carter), con la que se volverá a ver las caras en un nuevo enfrentamiento.
+
+En esta nueva aventura de fantasía, Alicia, con la ayuda de algunos nuevos amigos, tendrá que enfrentarse a distintos obstáculos para lograr su objetivo en esa realidad en la que todo es mágicamente posible o imposible.</p>
                     <a  class="button-books" download="Alicia a través del espejo" href="../src/02_ALICIA A TRAVES DEL ESPEJO.pdf">Descargar</a>
                     <a  class="button-books" href="#">Comprar</a>
                 </div>
