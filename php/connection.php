e1e597ccffcedff288dd92065570214ff65a6ecf
DemoNxriN <100371984+DemoNxriN@users.noreply.github.com>
2024-03-15 20:06:33 +0100

    Update

--- php/connection.php
+++ php/connection.php
@@ -1,13 +1,12 @@
 <?php
+
 $servername = "bbdd.bookhub.cat";
 $username = "ddb219390";
 $password = "lTDYST8S9KlIwU";
 $dbname = "ddb219390";
 
-// Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar conexión
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
